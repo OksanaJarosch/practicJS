db2dfde60aa9bb416737ff93a26433086d9390d8
Oksana Jarosch <ok.jarosch@gmail.com>
2023-07-15 09:49:08 +0200

    task1

---
+++ js/lessons1.js
@@ -0,0 +1,8 @@
+// ЗАДАЧА 1
+// НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
+const name = "ГАЛИНА";
+const age = 35;
+const language = "JAVASCRIPT";
+const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${language}`;
+
+console.log(message);
