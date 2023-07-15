b3e6d9722290521b79c73231d74ad0ac57a0e90b
Artemskyba <artemgennadievich@icloud.com>
2023-07-15 11:19:43 +0300

    task_4

--- js/lessons1.js
+++ js/lessons1.js
@@ -21,8 +21,23 @@
 // ЗАДАЧА 3
 // ПОРАХУЙТЕ СУМУ ТОВАРІВ У КОРЗИНІ . ПОТРІБНО ВВЕСТИ СУМУ 3-Х ТОВАРІВ І ПОРАХУВАТИ ЇХ СУМУ
 
-const tivyPrice = Number(prompt("Введіть ціну телевізору")) ;
-const telefonPrice = +prompt("Введіть ціну телефону");
-const tabletPrice = +prompt("Введіть ціну планшету");
-const message = tivyPrice + telefonPrice + tabletPrice;
-console.log(message);
+// const tivyPrice = Number(prompt("Введіть ціну телевізору")) ;
+// const telefonPrice = +prompt("Введіть ціну телефону");
+// const tabletPrice = +prompt("Введіть ціну планшету");
+// const message = tivyPrice + telefonPrice + tabletPrice;
+// console.log(message);
+
+// ЗАДАЧА 4
+// ВИКОРИСТОВУЮЧИ КОНСТРУКЦІЮ IF...ELSE , НАПИШИ КОД, ЯКИЙ БУДЕ ЗАПИТУВАТИ: 
+// "СКІЛЬКИ МІСЯЦІВ МАЄ РІК", ЯКЩО КОРИСТУВАЧ ВВОДИТЬ 12 ТО У КОНСОЛІ ПИШЕ ВІРНО,
+// ЯКЩО НЕ ПРАВИЛЬНО КОРИСТУВАЧ ВВІВ, ТОДІ У КОНСОЛІ ПИШЕ 
+// "НЕ ЗНАЄТЕ? 12"
+
+
+// const question = Number(prompt("СКІЛЬКИ МІСЯЦІВ МАЄ РІК"));
+
+// if (question === 12) {
+//     let message = alert(`Вірно`)
+// } else {
+//     message = alert(`НЕ ЗНАЄТЕ? 12`);
+// }
