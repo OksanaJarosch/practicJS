bc60187cbd76b2d202e1e2984d64a9d3821db018
Danylo2403 <daniilkon24@gmail.com>
2023-07-15 11:08:48 +0300

    task3

--- js/lessons1.js
+++ js/lessons1.js
@@ -10,10 +10,19 @@
 // ЗАДАЧА 2
 // ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ , ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
 
-const name = prompt("Give your name");
-const mail = prompt("Give your mail");
-const tel = prompt("Give your phone number");
+// const name = prompt("Give your name");
+// const mail = prompt("Give your mail");
+// const tel = prompt("Give your phone number");
 
-const message = `User ${name} uses ${mail} mail and ${tel} phone number.`;
+// const message = `User ${name} uses ${mail} mail and ${tel} phone number.`;
 
+// console.log(message);
+
+// ЗАДАЧА 3
+// ПОРАХУЙТЕ СУМУ ТОВАРІВ У КОРЗИНІ . ПОТРІБНО ВВЕСТИ СУМУ 3-Х ТОВАРІВ І ПОРАХУВАТИ ЇХ СУМУ
+
+const tivyPrice = Number(prompt("Введіть ціну телевізору")) ;
+const telefonPrice = +prompt("Введіть ціну телефону");
+const tabletPrice = +prompt("Введіть ціну планшету");
+const message = tivyPrice + telefonPrice + tabletPrice;
 console.log(message);
