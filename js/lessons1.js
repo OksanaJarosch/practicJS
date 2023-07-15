076007138f0131893046db00fd5a502a2e04af30
Arsentosh <doncenkoarsenij185@gmail.com>
2023-07-15 10:58:04 +0300

    Task 2

--- js/lessons1.js
+++ js/lessons1.js
@@ -1,8 +1,19 @@
 // ЗАДАЧА 1
 // НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
-const name = "ГАЛИНА";
-const age = 35;
-const language = "JAVASCRIPT";
-const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${language}`;
+// const name = "ГАЛИНА";
+// const age = 35;
+// const language = "JAVASCRIPT";
+// const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${language}`;
+
+// console.log(message);
+
+// ЗАДАЧА 2
+// ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ , ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
+
+const name = prompt("Give your name");
+const mail = prompt("Give your mail");
+const tel = prompt("Give your phone number");
+
+const message = `User ${name} uses ${mail} mail and ${tel} phone number.`;
 
 console.log(message);
