ed4531b5e1e083c3cc2b088b9a22d09c0e61b978
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2023-07-29 12:53:19 +0300

    lessons9

--- js/lessons2.js
+++ js/lessons2.js
@@ -201,3 +201,29 @@
 
 
 // console.log(courses);
+
+// ЗАДАЧА 9
+// Напишіть скрипт,для обєкта user в послідовності:
+
+//  добавляє поле mood із значенням 'happy'
+// замінює значення hobby на 'skydiving'
+// замінює значення  premium на false
+// виводить отримане обєкта user в форматі ключ:значення використовуючи  Object.keys() и for...of
+
+//  const user = {
+//    name: "Mango",
+//    age: 20,
+//    hobby: "html",
+//    premium: true,
+// };
+
+// user.mood = 'happy';
+// user.hobby = 'skydiving';
+// user.premium = false;
+
+// const userKey = Object.keys(user);
+// console.log(userKey);
+
+// for (let key of userKey) {
+//     console.log(`${key}: ${user[key]}`);    
+// }
