f669c07b7403f4d293171a2a1f6155c674bf2dd6
Artemskyba <artemgennadievich@icloud.com>
2023-08-12 10:55:37 +0300

    Artem_Skyba

--- js/lessons3.js
+++ js/lessons3.js
@@ -13,3 +13,19 @@
 // }
 
 // letMeSeeYourName(greet);
+// ЗАДАЧА 2
+// Напишіть дві функції:
+// makeProduct(name, price, callback) -приймає імя та ціну товару, а також колбек.Функція створює обєкт товару, добавляє йому унікальний ідентифікатор, в значення id і викликає колбек, передаючи йому створений обєкт.
+// showProduct(product) - колбек приймає обєкт продукта і логінує його в консоль;
+// makeProduct("Холодильник", 10000, logger);
+
+// const makeProduct = (name, price, callbcak) => {
+//   const product = { name, price, id: Date.now() }
+//   callbcak(product);
+// }
+
+// function showProduct(product) {
+//   console.log(product);
+// }
+
+// makeProduct("Холодильник", 10000, showProduct);
