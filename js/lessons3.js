f78710f26ab7e9f0aac5dbb867858fd79ecc8b3a
Artemskyba <artemgennadievich@icloud.com>
2023-08-12 12:31:11 +0300

    person

--- js/lessons3.js
+++ js/lessons3.js
@@ -163,3 +163,41 @@ const scientists = [
 
 // const isWorking = scientists.every(({ born }) => born >= 1800 && born <= 1882);
 // console.log(isWorking);
+
+// ЗАДАЧА 1
+// 1) Напишіть ф - ю конструктор яка створює обєкт person з такимим ключами:
+// const Person = {
+//   firstName,
+//   lastName,
+//   age,
+//   gender,
+//   interest,
+//   bio() {
+//     // виводить рядок Привіт {імя} мені {вік} років. Мені подобається {інтерес}
+//   },
+//   greeting() {
+//     // Привіт, я {імя}
+//   },
+// };
+
+function Person(firstName, lastName, age, gender, interest) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.gender = gender;
+  this.interest = interest;
+
+  Person.prototype.bio = function () {
+    console.log(`Привіт ${this.firstName} ${this.lastName} мені ${this.age} років. Мені подобається ${this.interest}`);
+  }
+
+  this.greeting= function () {
+    console.log(`Привіт, я ${this.firstName} ${this.lastName}`);
+  }
+}
+
+const myPerson = new Person("Artem", "Skyba", 27, "Male", "Programming");
+
+console.log(myPerson);
+myPerson.bio();
+myPerson.greeting();
